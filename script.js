fefc01c3b8030dcf113ebfd3b2e7cad01ec033a9
Lázaro <lazaro.2022315968@aluno.iffar.edu.br>
2024-08-26 17:06:59 -0300

    comeco do script

---
+++ script.js
@@ -0,0 +1,6 @@
+function salvarUsuario(event){
+    event.preventDefault();
+    let inputIDUsuario = document.getElementsByName("id_usuario");
+    let id_usuario = document.getElementsByName("id_usuario")[0].value;
+    console.log(id_usuario);
+}
